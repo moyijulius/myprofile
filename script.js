070329899dc677c7e8cca99636bbd48cdccf94eb
JuliusMoyi <116287251+moyijulius@users.noreply.github.com>
2024-11-22 15:35:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 const sections = document.querySelectorAll('section');
 const observer = new IntersectionObserver(
     (entries) => {
@@ -16,8 +15,17 @@ sections.forEach((section) => observer.observe(section));
 document.addEventListener('DOMContentLoaded', () => {
     const hamburger = document.querySelector('.hamburger');
     const mobileNav = document.querySelector('.mobile-nav');
+    const menuItems = document.querySelectorAll('.mobile-nav li a');
 
+    // Toggle the navigation menu
     hamburger.addEventListener('click', () => {
         mobileNav.classList.toggle('active');
+    });
+
+    // Close the navigation menu when a menu item is clicked
+    menuItems.forEach((item) => {
+        item.addEventListener('click', () => {
+            mobileNav.classList.remove('active');
+        });
     });
 });
